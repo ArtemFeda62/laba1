1ba633b3e01b2c184f2a13517361c2ac40b8e6d9
artem <fedorenkoartem95@gmail.com>
2025-09-04 18:45:15 +0700

    Логику добавил

--- ЛАБА1/Hero.cs
+++ ЛАБА1/Hero.cs
@@ -11,13 +11,12 @@ namespace ЛАБА1
         public int Id { get; set; }//айди
         public string Name { get; set; }//Имя
         public string Species { get; set; }//Раса
-        public int Genre { get; set; }//Гендер
-        public string Strange { get; set; }//Сила
+        public string Genre { get; set; }//Гендер
+        public int Strange { get; set; }//Сила
         public double Hp { get; set; } //Хп
         public string TypeOfDamage { get; set; }//Тип урона
-        public Hero(int id, string name, string species, int genre, string strange, string typeOfDamage)
+        public Hero( string name, string species, string genre, int strange, string typeOfDamage)
         {
-            Id = id;
             Name = name;
             Species = species;
             Genre = genre;
---
+++ ЛАБА1/Logic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛАБА1
+{
+    internal class Logic
+    {
+        private HerosLibrary library;
+        public Logic() 
+        {
+            library = new HerosLibrary();
+        }
+        public void CreateHero(string name, string genre, string species, double hp, string typeofdamage, int strange)
+        {
+            var Hero = new Hero(name, species, genre, strange, typeofdamage);
+            library.AddHero(Hero);
+        }
+    }
+}
