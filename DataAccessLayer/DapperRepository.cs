a5bb482339f42ae9268b37c40165826a10785d42
pavilll <Pavilitel12@gmail.com>
2025-10-31 12:18:46 +0700

    2 репозитория

---
+++ DataAccessLayer/DapperRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using DataAccessLayer.Dapper;
+using ЛАБА1;
+
+namespace DataAccessLayer.Dapper
+{
+    public class DapperRepository<T> : IRepository<T> where T : Hero, IDomainObject, new()
+    {
+        //строчка для подключение к базе данных
+        private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HeroDatabase;Integrated Security=True";
+        //метод чтобы подключится к этой базе
+        private IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+        public void Add(T entity)
+        {
+            //sql запрос данных с базы
+            var sql = @"INSERT INTO Heroes (Name, Species, Genre, Strange, Hp, TypeOfDamage) 
+                       VALUES (@Name, @Species, @Genre, @Strange, @Hp, @TypeOfDamage)";
+            using (var connection = CreateConnection())//подключение к базе, после чего подключение будет закрыто
+            {
+                connection.Execute(sql, entity);//благодаря этому методу мы маппим свойства объекта на парраметры entity
+            }
+        }
+        public void Delete(int id)
+        {
+            var sql = "DELETE FROM Heroes WHERE Id = @Id";//удаление по айди
+            using (var connection = CreateConnection())
+            {
+                connection.Execute(sql, new { Id = id });//маппим @id на свойство id 
+            }
+        }
+        public IEnumerable<T> ReadAll()
+        {
+            var sql = "SELECT * FROM Heroes";//возвращает все столбцы
+            using (var connection = CreateConnection())
+            {
+                return connection.Query<T>(sql);//этот метод для Select зарпосов,маппит все строчки на объекты Hero
+            }
+        }
+        public T ReadById(int id)
+        {
+            var sql = "SELECT * FROM Heroes WHERE Id = @Id";
+            using (var connection = CreateConnection())
+            {
+                return connection.QueryFirstOrDefault<T>(sql, new { Id = id });//вовзращает первую строку по айдишке
+            }
+        }
+        public void Update(T entity)
+        {
+            //устанавливает новые значения свойств при каком-то условии
+            var sql = @"UPDATE Heroes SET Name = @Name, Species = @Species, Genre = @Genre, 
+                       Strange = @Strange, Hp = @Hp, TypeOfDamage = @TypeOfDamage 
+                       WHERE Id = @Id";
+            using (var connection = CreateConnection())
+            {
+                connection.Execute(sql, entity);//меняет все свойства на новые
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/EntityRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ЛАБА1;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EntityRepository<T> : IRepository<T> where T : Hero, IDomainObject, new()
+    {
+        private readonly HeroContext _context;
+        public EntityRepository()
+        {//создание контекста БД
+            _context = new HeroContext();
+        }
+        public void Add(T entity)
+        {//добавление в бд и сохранение
+            _context.Heroes.Add(entity);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {//удаление по айдишке
+            var hero = _context.Heroes.FirstOrDefault(h => h.Id == id);
+            if (hero != null)
+            {
+                _context.Heroes.Remove(hero);
+                _context.SaveChanges();
+            }
+        }
+        public IEnumerable<T> ReadAll()
+        {//делаем список из dbset
+            return _context.Heroes.ToList() as IEnumerable<T>;
+        }
+        public T ReadById(int id)
+        {//вовзращаем объект по айди
+            return _context.Heroes.FirstOrDefault(h => h.Id == id) as T;
+        }
+        public void Update(T entity)
+        {//обновление объекта
+            var ent = _context.Heroes.Find(entity.Id);
+            if (ent != null)
+            {
+                _context.Entry(ent).CurrentValues.SetValues(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
