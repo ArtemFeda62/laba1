9a5d72267a15ce9d8800779ed362a42d07a2ac21
pavilll <Pavilitel12@gmail.com>
2025-11-13 22:59:54 +0700

    гойда!

---
+++ DataAccessLayer/SimpleConfigModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using DataAccessLayer;
+using DataAccessLayer.EntityFramework;
+using ЛАБА1;
+
+namespace BusinessLogicLayer
+{
+    public class SimpleConfigModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepository<Hero>>().To<EntityRepository<Hero>>().InSingletonScope();
+            Bind<Logic>().ToSelf();
+        }
+    }
+}
